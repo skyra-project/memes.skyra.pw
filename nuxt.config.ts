07013a7831bb07825d39c67dbfebfd22a769d054
Jeroen Claassens <support@favware.tech>
2023-05-22 22:55:00 +0200

    fix: add `json` and `wasm` to workbox glob patterns

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -88,7 +88,7 @@ export default defineNuxtConfig({
 		},
 		workbox: {
 			navigateFallback: '/',
-			globPatterns: ['**/*.{js,css,html,png,svg,ico,xml}']
+			globPatterns: ['**/*.{js,css,html,png,svg,ico,xml,json,wasm}']
 		},
 		manifest: {
 			background_color: '#292524',
