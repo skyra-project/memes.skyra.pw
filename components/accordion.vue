f707f033b7ccfbd060cb68bd3a92906a5d7c290b
Aura Román <kyradiscord@gmail.com>
2023-05-24 11:14:00 +0200

    refactor: improved accordion a11y and style

--- components/accordion.vue
+++ components/accordion.vue
@@ -1,10 +1,10 @@
 <template>
 	<div v-bind="$attrs">
-		<button class="w-full" @click="toggle()">
-			<slot name="header">Accordion</slot>
+		<button class="w-full p-2 rounded-xl" @click="toggle()" role="heading" :aria-level="level ?? 3">
+			<slot name="header">{{ header ?? 'Accordion' }}</slot>
 		</button>
 		<transition name="accordion" @before-enter="transitionClose" @enter="transitionOpen" @before-leave="transitionOpen" @leave="transitionClose">
-			<div v-show="show">
+			<div v-show="show" class="px-1 pb-1" :aria-expanded="show">
 				<slot></slot>
 			</div>
 		</transition>
@@ -12,6 +12,8 @@
 </template>
 
 <script setup lang="ts">
+defineProps<{ header?: string; level?: number }>();
+
 const [show, toggle] = useToggle(false);
 
 function transitionOpen(element: Element) {
